62f8393b22f07fa67540efe9dc7cd5c0aa346ab6
Reece Pennington <reecepennington@icloud.com>
2024-12-06 17:14:07 -0500

    got rid of NPC class

--- Main.java
+++ Main.java
@@ -5,8 +5,6 @@ public class Main{
 	
 
 	public static void main(String[] args) {
-		SocialStudiesQuestion ssQuestion = new SocialStudiesQuestion();
-		NonPlayerCharacter character = new NonPlayerCharacter();
 		
 		
 		Display display = new Display();
